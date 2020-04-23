010c26417262efd62ee3a44163d97cf115df9c64
Christina Landvytovych <Crofty.land@gmail.com>
2020-04-24 01:14:01 +0300

    fetch API

--- vanillaJS/JokeApp/src/js/index.js
+++ vanillaJS/JokeApp/src/js/index.js
@@ -1,46 +1,47 @@
 import '../style/styles.css';
-import heart from '../img/heart.svg';
+import request from './utilities/random.js';
+
 import chuck from '../img/chuck.png';
 
-document.getElementById('heart').src = heart;
 document.getElementById('chuck').src = chuck;
 
 var divs = ["Random", "Categories", "Search"];
 var visibleDivId = null;
+
 export function divVisibility(divId) {
-    if (visibleDivId === divId) {
-        visibleDivId = null;
-    } else {
-        visibleDivId = divId;
-    }
-    hideNonVisibleDivs();
+	if (visibleDivId === divId) {
+		visibleDivId = null;
+	} else {
+		visibleDivId = divId;
+	}
+	hideNonVisibleDivs();
 }
+
 function hideNonVisibleDivs() {
-    var i, divId, div;
-    for (i = 0; i < divs.length; i++) {
-        divId = divs[i];
-        div = document.getElementById(divId);
-        if (visibleDivId === divId) {
-            div.style.display = "block";
-        } else {
-            div.style.display = "none";
-        }
-    }
+	var i, divId, div;
+	for (i = 0; i < divs.length; i++) {
+		divId = divs[i];
+		div = document.getElementById(divId);
+		if (visibleDivId === divId) {
+			div.style.display = "block";
+		} else {
+			div.style.display = "none";
+		}
+	}
 }
 
- function displayRadioValue() {
-    document.getElementById("result").innerHTML = "";
-    var ele = document.getElementsByTagName('input');
+function displayRadioValue() {
+	document.getElementById("result").innerHTML = "";
+	var ele = document.getElementsByTagName('input');
 
-    for(i = 0; i < ele.length; i++) {
+	for (let i = 0; i < ele.length; i++) {
 
-        if(ele[i].type === "radio" ) {
+		if (ele[i].type === "radio") {
 
-            if(ele[i].checked)
-                document.getElementById("result").innerHTML
-                    += ele[i].name + " Value: "
-                    + ele[i].value + "<br>";
-        }
-    }
+			if (ele[i].checked)
+				document.getElementById("result").innerHTML
+					+= ele[i].name + " Value: "
+					+ ele[i].value + "<br>";
+		}
+	}
 }
-
