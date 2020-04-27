9a528a05173a2cae0cc1f5fbdfe989ccd02dca4f
Christina Landvytovych <Crofty.land@gmail.com>
2020-04-27 04:40:37 +0300

    render random jokes

--- vanillaJS/JokeApp/src/js/index.js
+++ vanillaJS/JokeApp/src/js/index.js
@@ -1,27 +1,29 @@
 import '../style/styles.css';
-import request from './utilities/request';
-
+import { request } from './utilities/request';
 
 import chuck from '../img/chuck.png';
 
+const fetchPromise = 'https://api.chucknorris.io/jokes/random';
 document.getElementById('chuck').src = chuck;
 
-
 let displayRadioValue = (function (target) {
-
 	let init = function() {
 		target.onclick = function(e) {
 			if (e.target.type == "radio") {
 				let matchingInput = target.querySelector('[data-value="'+ e.target.value +'"]');
 				switchToInput(matchingInput);
+        if (e.target.value === 'random'){
+          request(fetchPromise)
+        };
 
-			}
+      }
 		};
 	};
 
 	let switchToInput = function(input) {
 		let classInputs = target.querySelectorAll('.category__item--inner');
 		[].forEach.call(classInputs, function(input) {
+		  console.log(input)
 			input.classList.remove("visible");
 		});
 			input.classList.add("visible");
---
+++ vanillaJS/JokeApp/src/js/utilities/request.js
@@ -0,0 +1,55 @@
+const jokeCard = document.querySelector('.container');
+
+export const request = (fetchPromise) =>
+  fetch(fetchPromise)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => createJoke(data));
+
+  function createJoke(data) {
+    let fragment = document.createDocumentFragment();
+    let joke = document.createElement('div');
+    joke.classList.add('container');
+
+    let render = function (template, node) {
+      node.innerHTML = template;
+    };
+
+    const calculate = function() {
+      let previously = new Date(data.updated_at);
+      let now = Date.now();
+      let msPerMinute = 60 * 1000;
+      let msPerHour = msPerMinute * 60;
+
+      let elapsed = now - previously;
+
+      return Math.round(elapsed / msPerHour) + ' hours ago';
+    };
+
+    // language=HTML
+    let template = `
+       <div class="card">
+            <div class ="card__header">
+                <button class="card__headerBtn"></button>
+            </div>
+            <div class="card__body">
+                <div class="info">
+                    <div class="info__id">
+                       ID: <a href="${data.url}" class="info__link">${data.id}</a>
+                        <div class="info__text">${data.value}</div>
+                    </div>
+                </div>
+                    <div class="card__footer">
+                        <div class="update">Last update <time>${calculate()}</time></div>
+                    <div class="tag tag--min">${data.categories}</div>
+                </div>   
+            </div>
+       </div>`;
+    render(template, document.querySelector('#joke'));
+    fragment.appendChild(joke);
+    jokeCard.appendChild(fragment);
+}
+
+
+
