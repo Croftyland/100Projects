62cd8f4502044156b820d9b18b45208b7f226c63
Christina Landvytovych <Crofty.land@gmail.com>
2020-07-29 21:50:04 +0300

    fixed && tested

--- algorithm/practice/cracking/Array/String/URLify.js
+++ algorithm/practice/cracking/Array/String/URLify.js
@@ -1,10 +1,13 @@
 function URLify(str,len) {
- const convertString = str.trim().split('');
-
- for (let char in convertString) {
-     if(char[i] ===' ') {
-         char[i] += "%20";
+    const convertString = str.trim().split('');
+   
+    for (let char in convertString) {
+        if(convertString[char] ===' ') {
+            convertString[char] += "%20";
+        }
      }
-  }
-  return convertString.join('');
-}
+     return convertString.join('');
+   }
+
+
+//urlify('Mr John Smith    ', 13), 'Mr%20John%20Smith'
