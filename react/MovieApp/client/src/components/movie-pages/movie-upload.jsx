5d7dd5a02fdf76400257fcdaeaf81f9bbe8c34f3
Croftyland <crofty.land@gmail.com>
2020-09-04 20:28:24 +0300

    Fixed upload

--- react/MovieApp/client/src/components/movie-pages/movie-upload.jsx
+++ react/MovieApp/client/src/components/movie-pages/movie-upload.jsx
@@ -1,33 +1,84 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import Message from './movie-message';
 
 import axios from 'axios';
 
-
-const handleSelectFile = (event) => {
-    let file = event.currentTarget.files[0];
-
-    const data = new FormData();
-    data.append('file', file, file.name);
-    
-    return axios.post("http://localhost:5000/api/movie/upload", data, {
-      })
-      .then(res => {
-        console.log(res.data)
-      })
-}
-
 const UploadMovies = () => {
-        return (
-                <div className="container">
-                     <div className="form-group">
-                     <label htmlFor="file">File upload</label>
-                     <input id="file" name="file" type="file" onChange={(event) => {
-                     handleSelectFile(event);
-                  }} className="form-control" />
-              
-                </div>
-                <button type="submit" className="btn btn-primary">submit</button>
-              </div>
-            );
-          }
+  const [file, setFile] = useState('');
+  const [filename, setFilename] = useState('Upload file only in .txt format.');
+  const [uploadedFile, setUploadedFile] = useState({});
+  const [message, setMessage] = useState('');
+
+
+  const onChange = (event) => {
+    setFile(event.currentTarget.files[0]);
+    setFilename(event.currentTarget.files[0].name);
+  };
+
+  const handleSelectFile = async event => {
+    event.preventDefault();
+    const data = new FormData();
+    data.append('file', file);
+
+    try {
+      const res = await axios.post("http://localhost:5000/api/movie/upload", data, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        },
+      });
+
+      const { fileName, filePath } = res.data;
+
+      setUploadedFile({ fileName, filePath });
+
+      setMessage('File Uploaded');
+    } catch (err) {
+      if (err.response.status === 500) {
+      
+        setMessage('There was a problem with the server');
+      } else {
+        setMessage(err.response.data);
+      }
+    }
+  };
+
+  return (
+    <Fragment>
+      <form onSubmit={handleSelectFile}>
+        <div className='custom-file mb-4'>
+          <input
+            type='file'
+            className='custom-file-input'
+            id='customFile'
+            onChange={onChange}
+          />
+          <label className='custom-file-label' htmlFor='customFile'>
+            {filename} 
+          </label>
+          <p className="custom-file-text">
+            "Example of File" <br/>
+            Title: Blazing Saddles<br/>
+            Release Year: 1974<br/>
+            Format: VHS <br/>
+            Stars: Mel Brooks, Clevon Little, Harvey Korman</p>
+        </div>
+
+        <input
+          type='submit'
+          value='Upload'
+          className='btn btn-primary btn-block mt-4'
+        />
+      </form>
+      {uploadedFile ? (
+        <div className='row mt-5'>
+          <div className='col-md-6 m-auto'>
+            <h3 className='text-center'>{uploadedFile.fileName}</h3>
+          </div>
+        </div>
+      ) : null}
+    {message ? <Message msg={message} /> : null}
+    </Fragment>
+  );
+};
+
 export default UploadMovies;
