79066b57dd6bb59f2e7d6b3eb2a3fd2ebf8ef9c9
Croftyland <crofty.land@gmail.com>
2020-09-30 23:20:30 +0300

    config eslint

--- react/wwbm-clone/src/components/index.tsx
+++ react/wwbm-clone/src/components/index.tsx
@@ -1,7 +1,7 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import { App } from "./App";
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import { App } from './App'
 ReactDOM.render(
   <App userName="World" lang="TypeScript" />,
-  document.getElementById("output")
-);
+  document.getElementById('output'),
+)
