afdcde6996dd3980e59e197542c12c80cf00c8fc
Croftyland <crofty.land@gmail.com>
2020-10-03 04:42:20 +0300

    create components for app

---
+++ react/wwbm-clone/src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+// eslint-disable-next-line import/extensions
+import { App } from './components/App'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+)
