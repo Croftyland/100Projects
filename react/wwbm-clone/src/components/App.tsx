76e376793e60a9a41b5dfdec3ae7b28fdfa3bb17
Croftyland <crofty.land@gmail.com>
2020-10-06 11:58:00 +0300

    add app style

--- react/wwbm-clone/src/components/App.tsx
+++ react/wwbm-clone/src/components/App.tsx
@@ -1,19 +1,32 @@
-import * as React from 'react'
-import './App.css'
+import React, { useState } from 'react'
 import logo from '../images/hand.svg'
+
+// Styles
+import './App.css'
 export interface StartGameProps {
   phrase?: string
+}
+
+function onStart() {
+  console.log('start!')
 }
 
 export function App({
   phrase = 'Who wants to be a millionaire?',
 }: StartGameProps) {
   return (
-    <div>
-      <header className="App-header">
-        {phrase}
-      </header>
-      <img src={logo} className="App-logo" alt="logo" />
+    <div className="App-background ">
+      <div className="wrapper">
+        <div className="App-logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="App-header">
+          {phrase}
+        </div>
+        <div className="App-button">
+          <button type="button" className="App-button" onClick={onStart}>Start</button>
+        </div>
+      </div>
     </div>
   )
 }
